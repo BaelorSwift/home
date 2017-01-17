12a591f2e126d465b89e63f7acc1c9721346959a
Alex Forbes-Reed <github@alx.red>
2017-01-17 15:04:18 +0000

    api explorer on home page now works

---
+++ static/js/home.js
@@ -0,0 +1,29 @@
+var app = new Vue({
+	el: '#bae-app',
+	data: {
+		apiAddress: this.apiAddress,
+		apiPath: "songs/tim-mcgraw"
+	},
+	methods: {
+		makeRequest: function (event) {
+			var apiDemoResponse = document.getElementById("api-demo-response");
+			apiDemoResponse.className = "";
+
+			$.ajax({
+				url: this.apiAddress + this.apiPath,
+				method: 'GET'
+			}).done(function (data) {
+				document.getElementById("json-preview").textContent = JSON.stringify(data, null, 4);
+				document.getElementById("api-demo-response-header").textContent = "api response - http(200)";
+			}).fail(function (xhrErr, status, err) {
+				document.getElementById("api-demo-response-header").textContent = "api response - http(" + xhrErr.status + ")";
+				document.getElementById("json-preview").textContent = JSON.stringify(xhrErr.responseJSON, null, 4);
+			}).always(function () {
+				apiDemoResponse.className = " active";
+				
+				document.getElementById("api-demo-response").style.display = "block";
+				hljs.highlightBlock($('pre code')[0]);
+			});
+		}
+	}
+});
